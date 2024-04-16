b02f484765eafb9d163b42176b81aba0e4ef15ad
arvind808 <arvindbhadoriya808@gmail.com>
2024-04-16 17:19:02 +0530

    Add test case for handling multiple numbers with commas and new lines

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe StringCalculator do
     context 'when given an empty string' do
       it 'returns 0' do
         string = StringCalculator.new
-        expect(string.add("")).to eq(0)
+        expect(string.add('')).to eq(0)
       end
     end
 
@@ -21,6 +21,12 @@ RSpec.describe StringCalculator do
       it 'returns the sum of the numbers' do
         calculator = StringCalculator.new
         expect(calculator.add('1,2')).to eq(3)
+      end
+    end
+
+    context 'when given multiple numbers separated by commas and new lines' do
+      it 'returns the sum of the numbers' do
+        expect(StringCalculator.new.add("1\n2,3")).to eq(6)
       end
     end
   end
